f98b8bc0d37d24e4c56efd7bd48c5862c8af1e2b
Yiting Xiao <yitingxiao0531@gmail.com>
2024-03-13 16:52:46 -0400

    parse date format to meet sql format

--- server/controllers/proofreadController.ts
+++ server/controllers/proofreadController.ts
@@ -7,11 +7,52 @@ type proofreadController = {
     postProofreadInfo: postProofreadInfo;
 };
 
+type months = {
+  Jan: string,
+  Feb: string,
+  Mar: string,
+  Apr: string,
+  May: string,
+  Jun: string,
+  Jul: string,
+  Aug: string,
+  Sep: string,
+  Oct: string,
+  Nov: string,
+  Dec: string,
+}
+
+type dateArr = string[];
 const proofreadController = {
     postProofreadInfo: async (req: Request, res: Response, next: NextFunction) => {
       try {
-        // const d = new Date();
-        // const date = d.toDateString();
+        const d = new Date();
+        const date = d.toDateString();
+        const months: months = {
+         Jan: '01',
+         Feb: '02',
+         Mar: '03',
+         Apr: '04',
+         May: '05',
+         Jun: '06',
+         Jul: '07',
+         Aug: '08',
+         Sep: '09',
+         Oct: '10',
+         Nov: '11',
+         Dec: '12',
+         }
+        
+        const dateArr: dateArr = date.split(' ');
+        dateArr.splice(0, 1);
+       
+       for (let i = 0; i < dateArr.length; i++){
+         console.log('date[i]', dateArr[i])
+         if (months.hasOwnProperty(dateArr[i])){
+           dateArr[i] = months[dateArr[i]];
+         }
+       }
+       let correctDate = dateArr[2]+'-'+dateArr[1]+'-'+dateArr[0];
         // const { id, proofreader, filename, wordcount, date } = req.body;
         // const values = [ id, proofreader, filename, wordcount, date ];
         // const value = [date];
