4e386b856166d12ff21b99168a8f8adcc3c8af8d
Yiting Xiao <yitingxiao0531@gmail.com>
2024-03-17 16:30:28 -0400

    fix index.tsx file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import App from "./components/App";
 import { createRoot } from "react-dom/client";
 import "./static/styles.scss";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.tsx',
+    entry: './src/index.js',
     output: {
         filename:'bundle.js',
         path: path.resolve(__dirname, 'dist'),
@@ -30,6 +30,16 @@ module.exports = {
       ],
       module: {
         rules: [
+          {
+            test: /\.(?:js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-react', '@babel/preset-env']
+              }
+            },
+          },  
           {
             test: /\.(ts|tsx)$/,
             exclude: /node_modules/,
