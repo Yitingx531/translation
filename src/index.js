8e426e0c76c9123a9c746a4c162ebbc026236b98
Yiting Xiao <yitingxiao0531@gmail.com>
2024-03-17 19:48:32 -0400

    make date global as a prop

--- src/index.js
+++ src/index.js
@@ -7,5 +7,28 @@ const jsx_runtime_1 = require("react/jsx-runtime");
 const App_1 = __importDefault(require("./components/App"));
 const client_1 = require("react-dom/client");
 require("./static/styles.scss");
+const d = new Date();
+const date = d.toDateString();
+const months = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12',
+};
+const dateArr = date.split(' ');
+dateArr.splice(0, 1);
+for (let i = 0; i < dateArr.length; i++) {
+    if (months.hasOwnProperty(dateArr[i])) {
+    }
+}
+let correctDate = dateArr[1] + ' - ' + dateArr[0] + ' - ' + dateArr[2];
 const root = (0, client_1.createRoot)(document.getElementById('root'));
-root.render((0, jsx_runtime_1.jsx)(App_1.default, {}));
+root.render((0, jsx_runtime_1.jsx)(App_1.default, { correctDate: correctDate }));
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,37 @@ import App from "./components/App";
 import { createRoot } from "react-dom/client";
 import "./static/styles.scss";
 
+
+type prop = {
+    correctDate: string,
+};
+type dateArr = string[];
+type months = {
+    [key: string]: string
+};
+
+const d = new Date();
+const date = d.toDateString();
+const months: months = {
+     Jan: '01',
+     Feb: '02',
+     Mar: '03',
+     Apr: '04',
+     May: '05',
+     Jun: '06',
+     Jul: '07',
+     Aug: '08',
+     Sep: '09',
+     Oct: '10',
+     Nov: '11',
+     Dec: '12',
+}  
+const dateArr: dateArr = date.split(' ');
+dateArr.splice(0, 1);
+for (let i = 0; i < dateArr.length; i++){
+  if (months.hasOwnProperty(dateArr[i])){
+}
+}
+let correctDate: string = dateArr[1]  + ' - '+ dateArr[0] + ' - ' + dateArr[2];
 const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(<App correctDate={correctDate}/>);
